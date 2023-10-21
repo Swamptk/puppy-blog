4c87af182aa00cf6fcb679fea46ddd7dc69b93b6
valero <evalerovilella@gmail.com>
2023-10-21 20:55:12 +0200

    Added new db declaration style + added db to gitignore

--- project/__init__.py
+++ project/__init__.py
@@ -1,4 +1,5 @@
 import os
+from sqlalchemy.orm import declarative_base
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -8,18 +9,22 @@ from flask_migrate import Migrate
 base_path = os.path.abspath(os.path.dirname(__file__))
 db_path = "sqlite:///" + os.path.join(base_path, "database.db")
 
+##### Base Model
+Base = declarative_base()
+db = SQLAlchemy(model_class=Base)
+
 ##### App
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "mysecretkey"
 app.config['SQLALCHEMY_DATABASE_URI'] = db_path
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-db = SQLAlchemy(app)
+db.__init__(app)
 Migrate(app,db)
 
 ##### Login
 login_manager = LoginManager()
 login_manager.init_app(app)
-login_manager.login_view = "users.login"
+login_manager.login_view = "users.login" # type: ignore
 
 ##### BluePrints
 from project.core.views import core
@@ -36,4 +41,4 @@ app.register_blueprint(error_pages)
 with app.app_context():
     db.create_all()
 
-app.logger.info("App started successfully", flush=True)
+app.logger.info("App started successfully")
