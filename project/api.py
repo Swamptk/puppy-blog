e5eb814b3e5a4fa16c03b75d2face59f9ebe36db
valero <evalerovilella@gmail.com>
2023-10-22 23:05:53 +0200

    Added documentation to the api and utilities

--- project/api.py
+++ project/api.py
@@ -6,8 +6,19 @@ from project import db
 from datetime import datetime
 
 
+# The `UserPostsApi` class is a Flask resource that retrieves all blog posts associated with a given
+# username.
 class UserPostsApi(Resource):
-    def get(self, username):
+    def get(self, username:str):
+        """
+        The function retrieves all blog posts associated with a given username and returns them as a
+        JSON response.
+        
+        :param username: The `username` parameter is a string that represents the username of a user
+        :return: The code is returning a response containing JSON data. If the user has no posts, the
+        response will include the message "user has no posts yet". If the user has posts, the response
+        will include a list of JSON objects representing each post.
+        """
         user: User = User.query.filter_by(username=username).one_or_404()
         posts: list[BlogPost] = BlogPost.query.filter_by(user_id=user.id).all()
         if len(posts) == 0:
@@ -15,21 +26,47 @@ class UserPostsApi(Resource):
         return make_response(jsonify([post.json() for post in posts]))
 
 
+# The `ManageUsersApi` class provides methods to retrieve and delete user information from a database.
 class ManageUsersApi(Resource):
-    def get(self, username):
+    def get(self, username: str):
+        """
+        The function retrieves a user with a specific username and returns their information in JSON
+        format.
+        
+        :param username: The `username` parameter is the username of the user we want to retrieve from
+        the database
+        :return: a response object that contains the JSON representation of the user object.
+        """
         user: User = User.query.filter_by(username=username).one_or_404()
         return make_response(jsonify(user.json()))
 
     # TODO: only available with jwt_auth
-    def delete(self, username):
+    def delete(self, username: str):
+        """
+        The `delete` function deletes a user from the database based on their username and returns a
+        success message.
+        
+        :param username: The `username` parameter is a string that represents the username of the user
+        you want to delete from the database
+        :return: a response object with a JSON payload indicating that the deletion was successful. The
+        JSON payload includes a "success" key with the value "Deleted successfully."
+        """
         user: User = User.query.filter_by(username=username).one_or_404()
         db.session.delete(user)
         db.session.commit()
         return make_response(jsonify(success = "Deleted successfully."))
 
 
+# The CreateUserApi class is a resource for creating user accounts.
 class CreateUserApi(Resource):
     def post(self):
+        """
+        The above function is a POST request handler that creates a new user with the provided data and
+        returns a response.
+
+        :return: a response object with JSON data. The JSON data includes a success message and the
+        user's information in JSON format. The response status code is 200.
+        """
         json: dict | None = request.json
         if not json:
             resp_data = jsonify(error = "no data provided.")
@@ -61,10 +98,21 @@ class CreateUserApi(Resource):
         db.session.commit()
         resp_data = jsonify({"success": "user created successfully", "user": user.json()})
         return make_response(resp_data, 200)
-
+    
+# The CreatePostApi class is a resource for creating posts.
 class CreatePostApi(Resource):
     # TODO: only available with jwt_auth
     def post(self):
+        """
+        The above function is a POST request handler that creates a new blog post with the provided
+        data.
+        
+        :return: The code is returning a response with JSON data. If the request does not contain any
+        JSON data, a response with an error message "no data provided." is returned with a status code
+        of 404. If the request contains the required fields (user_id, title, and text), a new BlogPost
+        object is created and added to the database. If the optional field created_at is provided and in
+        the given format, it sets it as the data of creation of the post.
+        """
         json = request.json
         if not json: 
             resp_data = jsonify(error = "no data provided.")
--- utils/post_gen.py
+++ utils/post_gen.py
@@ -6,6 +6,19 @@ import requests
 
 
 def create_post(base_url: str, user_id: int, post: dict[str, str]):
+    """
+    The function `create_post` sends a POST request to a specified API endpoint with a user ID and a
+    post dictionary as JSON data.
+    
+    :param base_url: The `base_url` parameter is a string that represents the base URL of the API
+    endpoint you want to send the POST request to. It should include the protocol (e.g., "http://" or
+    "https://") and the domain name or IP address
+    :param user_id: The `user_id` parameter is an integer that represents the ID of the user who is
+    creating the post
+    :param post: The `post` parameter is a dictionary that contains the details of the post. It should
+    have the following keys:
+    :return: the response object from the POST request.
+    """
     api_url = "/api/createpost"
     url = base_url + api_url
     post["user_id"] = user_id  # type: ignore
@@ -14,6 +27,16 @@ def create_post(base_url: str, user_id: int, post: dict[str, str]):
 
 
 def create_posts(base_url:str, posts_file: str):
+    """
+    The function `create_posts` creates posts by randomly selecting a user ID from a list of user IDs
+    and then calling the `create_post` function with the selected user ID and the post data.
+    
+    :param base_url: The base URL is the base address of the API endpoint where you want to create the
+    posts. It should include the protocol (e.g., "http://") and the domain name (e.g., "example.com")
+    :param posts_file: The `posts_file` parameter is the file path to a JSON file that contains the
+    posts data. This file should have a key called "posts" which contains a list of post objects
+    :return: The function does not explicitly return anything.
+    """
     with app.app_context():
         users = User.query.all()
         if len(users) == 0:
